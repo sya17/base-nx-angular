bb72e6b28883ca0defe6912ab74cbeb684cdaaa9
Sarip Hidayatullah <102409123+sya17@users.noreply.github.com>
2025-07-09 23:27:33 +0700

    update
    add home & not-found pages

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {}
---
+++ src/app/pages/not-found/not-found.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [CommonModule],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss',
+})
+export class NotFound {}
